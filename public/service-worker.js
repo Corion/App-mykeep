f06eb92243adad12ace5abd8df7131484e98987b
Max Maischein <corion@corion.net>
2017-03-31 22:24:01 +0200

    Update service-worker, note URL

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+// https://bitsofco.de/bitsofcode-pwa-part-1-offline-first-with-service-worker/
 // Import the Service Worker Toolbox file
 importScripts('javascripts/sw-toolbox.js');
 
@@ -10,4 +11,21 @@ const precacheFiles = [
 ];
 
 // Precache the files
-toolbox.precache(precacheFiles)  
+toolbox.precache(precacheFiles);
+
+self.addEventListener('fetch', function(event) {  
+    // Respond to the document with what is returned from 
+    event.respondWith(
+        // 1. Check the cache if a file matching that request is available
+        caches.match(event.request).then((response) => {
+            // 2. If it is, respond to the document with the file from the cache        
+            if ( response ) {
+                return response;
+            };
+
+            / 3. If it isn’t, fetch the file from the network and respond to the document with the fetched file
+            return fetch(event.request);
+
+        });
+    );
+});
