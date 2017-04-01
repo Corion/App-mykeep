cbeae8f98fea6da01101ef344952645b647efdc6
Max Maischein <corion@corion.net>
2017-04-01 14:34:16 +0200

    Load list from localforage
    This means we lose the upstream connectivity for now

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,76 @@
 // https://bitsofco.de/bitsofcode-pwa-part-1-offline-first-with-service-worker/
 // Import the Service Worker Toolbox file
 'use strict';
-importScripts('javascripts/sw-toolbox.js');
+importScripts(
+    './javascripts/sw-toolbox.js',
+    './javascripts/localforage.js'
+);
+self.toolbox.options.debug = true;
 
 const precacheFiles = [  
-    './',
-    './index.html',
+    //'./',
+    //'./index.html',
     './javascripts/jquery-3.1.1.min.js',
     './javascripts/handlebars-v4.0.5.js',
     './javascripts/app.js'
 ];
 
+// Start up immediately, replacing old instances
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
+});
+
+// All pages can use us immediately
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+  console.log('(sw) Finally active. Ready to start serving content!');  
+});
+
 // Precache the files
-toolbox.precache(precacheFiles);
+self.toolbox.precache(precacheFiles);
 
-var cannedNotes = {notes:[
-{"id":1,"title":"Test title","bgcolor":"#FFE040","modifiedAt":0,"text":"Test text (sw)"},
-{"id":2,"title":"Test title 2","bgcolor":"#FFE040","modifiedAt":0,"text":"Test text 3 (sw)"}
-]};
+localforage.config({
+    "name": "mykeep",
+    "storename": "notes",
+    "description": "Local cache of App::mykeep notes"
+});
 
-toolbox.router.get("/notes/list", function(request, values,options) {
-    //return Promise.resolve(cannedNotes);
-    return new Response(JSON.encode(cannedNoted), {
-        headers: { 'Content-Type': 'application/json' }
+// Return full notes for the time being
+function localNotes() {
+    return localforage.keys().then(function(keys){
+        console.log(keys);
+        var fullNotes = [];
+        for( var k in keys ) {
+            fullNotes.push( localforage.getItem(k) );
+        };
+        return Promise.all(fullNotes)
     });
-}, {});
+}
+
+self.toolbox.router.get("/notes/list", function(request, values,options) {
+    console.log("(sw) fetch notes list called");
+    //var payload = JSON.stringify(cannedNotes);
+    
+    // If we have a network connection, also attempt a sync to fetch the
+    // the real list
+    
+    return localNotes().then(function(cannedNotes) {
+        var payload = JSON.stringify(cannedNotes);
+        console.log("(sw): local JSON notes" + payload );
+        return new Response(payload, {
+            "status": 200,
+            "headers": { 'Content-Type': 'application/json' }
+        });
+    });
+});
+
+// Automatically store all notes we download in the cache
+// Even incomplete items, so we know what to fetch later
+
+self.toolbox.router.default = function(request, values,options) {
+    console.log("(sw) Default fetch called");
+    return fetch(request);
+};
 
 /*
 self.addEventListener('message', function(event) {
