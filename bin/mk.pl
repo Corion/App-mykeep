48eff1f40b63b5ba30be0da7ec7028fcfa1614b7
Max Maischein <corion@corion.net>
2017-08-07 14:38:02 +0200

    Allow multiple labels
    ... even if we don't use it at all

--- bin/mk.pl
+++ bin/mk.pl
@@ -33,7 +33,7 @@ GetOptions(
     # Should we have an "init" action that sets up
     # a new note directory and config file?
 
-    't|label:s'  => \my $label,
+    't|label:s'  => \my @label,      # show only this label
 )
 or pod2usage(2);
 
@@ -67,7 +67,7 @@ if( @ARGV ) {
 if( $edit_note or $append_note ) {
 
     my @notes;
-    if( @note_body or $label ) {
+    if( @note_body or @label ) {
         my $search;
         if( $edit_note ) {
             $search = join " ", @note_body;
@@ -75,7 +75,7 @@ if( $edit_note or $append_note ) {
             $search = shift @note_body;
         };
         # Search title and body
-        @notes = $client->list_items( text => $search, label => $label );
+        @notes = $client->list_items( text => $search, label => @label );
     };
 
     if( @notes == 1 ) {
@@ -113,7 +113,7 @@ if( $edit_note or $append_note ) {
 # split out actions into separate packages, like all the cool kids do?
 if( $list_notes ) {
     my $search = join " ", @note_body;
-    my @notes = $client->list_items( text => $search, label => $label );
+    my @notes = $client->list_items( text => $search, label => @label );
     #@notes = $client->sync_items( update_local => 1, update_remote => 1 );
 
     display_notes( @notes );
